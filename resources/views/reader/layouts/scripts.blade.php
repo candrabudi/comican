1eeb4bbffa1da2080f4b33cfcd7bb4a0a1b86d32
candrabudi <candrasachio@gmail.com>
2023-12-24 13:57:24 +0700

    remove file not use

--- resources/views/reader/layouts/scripts.blade.php
+++
@@ -1,9 +0,0 @@
-<noscript>
-    <link rel='stylesheet' id='wp-block-library-css' href="{{ asset('mangareader/css/style.min.css')}}" type='text/css' media='all' />
-    <link data-minify="1" rel='stylesheet' id='owl-carousel-css' href="{{ asset('mangareader/css/owl.carousel.css')}}" type='text/css'
-        media='all' />
-    <link data-minify="1" rel='stylesheet' id='icon-css' href="{{ asset('mangareader/css/all.css') }}" type='text/css' media='all' />
-    <link data-minify="1" rel='stylesheet' id='theme-css' href={{ asset('mangareader/css/front.style.css') }}" type='text/css' media='all' />
-    <link data-minify="1" rel='stylesheet' id='dark-theme-css' href="{{ asset('css/front.dark.css') }}" type='text/css'
-        media='all' />
-</noscript>
