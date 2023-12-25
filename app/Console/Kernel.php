353a6f86c51183bd05c9d1560df51629af09a36c
candrabudi <candrasachio@gmail.com>
2023-12-25 17:58:45 +0700

    fix schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('schedule:run')->everyMinute();
+        $schedule->command(new CrawlChapterJob())->everyMinute();
         $schedule->command('cron:log')
                ->everyMinute();
     }
