93d5e0dbf3836337be40d178eea95bd71e6f52f9
candrabudi <candrasachio@gmail.com>
2023-12-26 14:15:06 +0700

    fix list chapter

--- app/Models/Comic.php
+++ app/Models/Comic.php
@@ -35,8 +35,11 @@ class Comic extends Model
     public function comicChapterAll()
     {
         return $this->hasMany(ComicChapter::class, 'comic_id', 'id')
-            ->selectRaw("*, CAST(REGEXP_REPLACE(chapter_number_original, '[^0-9]', '') AS UNSIGNED) AS numeric_chapter_number")
-            ->orderByRaw("CAST(REGEXP_REPLACE(chapter_number_original, '[^0-9]', '') AS UNSIGNED) + 0 DESC");
+            ->selectRaw("*, 
+                CAST(SUBSTRING_INDEX(REGEXP_REPLACE(chapter_number_original, '[^0-9\.]', ''), '.', 1) AS UNSIGNED) AS int_part,
+                CAST(SUBSTRING_INDEX(REGEXP_REPLACE(chapter_number_original, '[^0-9\.]', ''), '.', -1) AS UNSIGNED) AS decimal_part")
+            ->orderByDesc('int_part')
+            ->orderByDesc('decimal_part');
     }
     public function comicViews(): HasMany
     {
